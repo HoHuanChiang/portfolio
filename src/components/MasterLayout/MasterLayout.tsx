699d5b7413fbc0dd7af93fd84b4b4844fc7f05df
Ho-Huan Chiang <hc57@rice.edu>
2023-04-26 00:22:02 -0400

    add switch router

--- src/components/MasterLayout/MasterLayout.tsx
+++ src/components/MasterLayout/MasterLayout.tsx
@@ -1,20 +1,45 @@
 import React from "react";
 import "./MasterLayout.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "../Home/Home";
+import WorkExperience from "../WorkExperience/WorkExperience";
+import Project from "../Project/Project";
+import Resume from "../Resume/Resume";
 
 const MasterLayout = () => {
     return (
         <div className={"main"}>
             <div className={"topNavbar"}>Ho Huan Chiang</div>
             <div className={"content"}>
-                <div className={"leftNavbar"}>
-                    <nav>
-                        <div>Home</div>
-                        <div>Work Experience</div>
-                        <div>Projects</div>
-                        <div>Resume</div>
-                    </nav>
-                </div>
-                <div className={"innerContent"}></div>
+                <Router>
+                    <div className={"leftNavbar"}>
+                        <nav>
+                            <Link to={"/"}>
+                                <div>Home</div>
+                            </Link>
+                            <Link to={"/workExperience"}>
+                                <div>Work Experience</div>
+                            </Link>
+                            <Link to={"/project"}>
+                                <div>Projects</div>
+                            </Link>
+                            <Link to={"/resume"}>
+                                <div>Resume</div>
+                            </Link>
+                        </nav>
+                    </div>
+                    <div className={"innerContent"}>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route
+                                path="workExperience"
+                                element={<WorkExperience />}
+                            />
+                            <Route path="project" element={<Project />} />
+                            <Route path="resume" element={<Resume />} />
+                        </Routes>
+                    </div>
+                </Router>
             </div>
         </div>
     );
