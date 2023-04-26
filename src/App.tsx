94096e7c5cbe3c030d2e1eb39f435acc64d90e06
Ho-Huan Chiang <hc57@rice.edu>
2023-04-26 14:56:43 -0400

    show selected tab when user is on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import MasterLayout from "./components/MasterLayout/MasterLayout";
 
 function App() {
     return (
         <div>
-            <MasterLayout />
+            <Router>
+                <MasterLayout />
+            </Router>
         </div>
     );
 }
--- src/components/MasterLayout/MasterLayout.tsx
+++ src/components/MasterLayout/MasterLayout.tsx
@@ -1,45 +1,95 @@
 import React from "react";
 import "./MasterLayout.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    useLocation,
+} from "react-router-dom";
 import Home from "../Home/Home";
 import WorkExperience from "../WorkExperience/WorkExperience";
 import Project from "../Project/Project";
 import Resume from "../Resume/Resume";
 
+interface CustomPathProps {
+    path: string;
+    isCurrentPage: boolean;
+    name: string;
+}
+
+enum Path {
+    Home = "/",
+    WorkExperience = "/workExperience",
+    Projects = "/project",
+    Resume = "/resume",
+}
+
 const MasterLayout = () => {
+    const location = useLocation();
+
+    const isCurrentPage = (path: Path): boolean => {
+        const currentPathName = location.pathname;
+        return path.toLocaleLowerCase() === currentPathName.toLocaleLowerCase();
+    };
+
+    const paths: CustomPathProps[] = [
+        {
+            path: Path.Home,
+            isCurrentPage: isCurrentPage(Path.Home),
+            name: "Home",
+        },
+        {
+            path: Path.WorkExperience,
+            isCurrentPage: isCurrentPage(Path.WorkExperience),
+            name: "Work Experience",
+        },
+        {
+            path: Path.Projects,
+            isCurrentPage: isCurrentPage(Path.Projects),
+            name: "Projects",
+        },
+        {
+            path: Path.Resume,
+            isCurrentPage: isCurrentPage(Path.Resume),
+            name: "Resume",
+        },
+    ];
+
     return (
         <div className={"main"}>
             <div className={"topNavbar"}>Ho Huan Chiang</div>
             <div className={"content"}>
-                <Router>
-                    <div className={"leftNavbar"}>
-                        <nav>
-                            <Link to={"/"}>
-                                <div>Home</div>
-                            </Link>
-                            <Link to={"/workExperience"}>
-                                <div>Work Experience</div>
-                            </Link>
-                            <Link to={"/project"}>
-                                <div>Projects</div>
-                            </Link>
-                            <Link to={"/resume"}>
-                                <div>Resume</div>
-                            </Link>
-                        </nav>
-                    </div>
-                    <div className={"innerContent"}>
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route
-                                path="workExperience"
-                                element={<WorkExperience />}
-                            />
-                            <Route path="project" element={<Project />} />
-                            <Route path="resume" element={<Resume />} />
-                        </Routes>
-                    </div>
-                </Router>
+                <div className={"leftNavbar"}>
+                    <nav>
+                        {paths.map((pathProps) => {
+                            return (
+                                <Link to={pathProps.path}>
+                                    <div
+                                        className={
+                                            pathProps.isCurrentPage
+                                                ? "isSelected"
+                                                : ""
+                                        }
+                                    >
+                                        {pathProps.name}
+                                    </div>
+                                </Link>
+                            );
+                        })}
+                    </nav>
+                </div>
+                <div className={"innerContent"}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route
+                            path="workExperience"
+                            element={<WorkExperience />}
+                        />
+                        <Route path="project" element={<Project />} />
+                        <Route path="resume" element={<Resume />} />
+                    </Routes>
+                </div>
             </div>
         </div>
     );
