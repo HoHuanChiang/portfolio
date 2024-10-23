8900121a21b68d8212df2180c987a220c1c67683
Ho Huan Chiang <chianglars@gmail.com>
2024-10-23 00:20:12 -0700

    add tabapay work experience

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,19 +3,19 @@ import EngineerTextAnimationGroup from "./EngineerTextAnimationGroup/EngineerTex
 import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className={"homeContainer"} id={"homeSection"}>
-            <div className={"aboutMe"}>
-                <EngineerTextAnimationGroup
-                    textGroup={[
-                        "Hi, I'm Ho-Huan Chiang or you can call me Lars!",
-                        "I am an experienced software engineer actively seeking new opportunities in the field.",
-                        "I am constantly striving to learn and grow in my field, and I am excited to bring my expertise to new projects and challenges.",
-                    ]}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className={"homeContainer"} id={"homeSection"}>
+      <div className={"aboutMe"}>
+        <EngineerTextAnimationGroup
+          textGroup={[
+            "Hi, I'm Ho-Huan Chiang or you can call me Lars!",
+            "I am an experienced software engineer currently working at Tabapay.",
+            "I am constantly striving to learn and grow in my field, and I am excited to bring my expertise to new projects and challenges.",
+          ]}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/WorkExperience/WorkExperience.tsx
+++ src/components/WorkExperience/WorkExperience.tsx
@@ -4,48 +4,46 @@ import { setAnimation } from "./WorkExperience.trigger";
 import { COMPANIES, CompanyInfo } from "./WorkExperience.util";
 
 const WorkExperience = () => {
-    const ref = useRef<HTMLDivElement | null>(null);
-    const windowSize = useRef([window.innerWidth, window.innerHeight]);
-    React.useEffect(() => {
-        setAnimation(ref.current, windowSize.current[0]);
-    }, [windowSize]);
+  const ref = useRef<HTMLDivElement | null>(null);
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+  React.useEffect(() => {
+    setAnimation(ref.current, windowSize.current[0]);
+  }, [windowSize]);
 
-    const renderCompany = (company: CompanyInfo, showTimelineLink: boolean) => {
-        return (
-            <div className={`${company.id} companyContainer`}>
-                <div className={"workTimeline"} id={`${company.id}Timeline`}>
-                    <div className={"stick"} />
-                    <div className={"topDot"} />
-                    <div className={"bottomDot"} />
-                    <span className={"topDate"}>{company.endDate}</span>
-                    <span className={"bottomDate"}>{company.startDate}</span>
-                    {showTimelineLink && <div className={"freeTimeline"} />}
-                </div>
-                <div className={"linkToTimeline"} />
-                <div className={"company"}>
-                    <div className={"companyContent"}>
-                        <div className={"title"}>{company.name}</div>
-                        <div className={"position"}>
-                            <div>{company.title}</div>
-                            <div>{company.location}</div>
-                        </div>
-                        <div>{company.description}</div>
-                    </div>
-                </div>
-            </div>
-        );
-    };
-
+  const renderCompany = (company: CompanyInfo, showTimelineLink: boolean) => {
     return (
-        <div className={"workEpContainer"} ref={ref} id={"workSection"}>
-            <div className={"sectionTitle"}>Work Experience</div>
-            <div className={"companies"}>
-                {COMPANIES.map((company, index) =>
-                    renderCompany(company, index !== COMPANIES.length - 1)
-                )}
-            </div>
+      <div className={`${company.id} companyContainer`}>
+        <div className={"workTimeline"} id={`${company.id}Timeline`}>
+          <div className={"stick"} />
+          <div className={"topDot"} />
+          <div className={"bottomDot"} />
+          <span className={"topDate"}>{company.endDate}</span>
+          <span className={"bottomDate"}>{company.startDate}</span>
+          {showTimelineLink && <div className={"freeTimeline"} />}
         </div>
+        <div className={"linkToTimeline"} />
+        <div className={"company"}>
+          <div className={"companyContent"}>
+            <div className={"title"}>{company.name}</div>
+            <div className={"position"}>
+              <div>{company.title}</div>
+              <div>{company.location}</div>
+            </div>
+            <div>{company.description}</div>
+          </div>
+        </div>
+      </div>
     );
+  };
+
+  return (
+    <div className={"workEpContainer"} ref={ref} id={"workSection"}>
+      <div className={"sectionTitle"}>Work Experience</div>
+      <div className={"companies"}>
+        {COMPANIES.map((company, index) => renderCompany(company, index !== COMPANIES.length - 1))}
+      </div>
+    </div>
+  );
 };
 
 export default WorkExperience;
